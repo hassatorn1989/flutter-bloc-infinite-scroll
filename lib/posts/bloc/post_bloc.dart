e96a3b5cb3d7fcb3da5d1863f7d578bd261523f8
hassatorn1989 <hassatorn2532@gmail.com>
2023-04-28 16:23:34 +0700

    no message

--- lib/posts/bloc/post_bloc.dart
+++ lib/posts/bloc/post_bloc.dart
@@ -24,35 +24,37 @@ class PostBloc extends Bloc<PostEvent, PostState> {
     required this.httpClient,
   }) : super(const PostState()) {
     on<PostFetched>(
-      _onPostFetched,
+      (event, emit) async {
+        if (state.hasReachedMax) return;
+        try {
+          if (state.status == PostStatus.initial) {
+            final posts = await _fetchPosts();
+            return emit(state.copyWith(
+              status: PostStatus.success,
+              posts: posts,
+              hasReachedMax: false,
+            ));
+          }
+          final posts = await _fetchPosts(state.posts.length);
+          emit(posts.isEmpty
+              ? state.copyWith(hasReachedMax: true)
+              : state.copyWith(
+                  status: PostStatus.success,
+                  posts: List.of(state.posts)..addAll(posts),
+                  hasReachedMax: false,
+                ));
+        } catch (_) {
+          emit(state.copyWith(status: PostStatus.failure));
+        }
+      },
       transformer: throttleDroppable(throttleDuration),
     );
   }
 
-  Future<void> _onPostFetched(
-      PostFetched event, Emitter<PostState> emit) async {
-    if (state.hasReachedMax) return;
-    try {
-      if (state.status == PostStatus.initial) {
-        final posts = await _fetchPosts();
-        return emit(state.copyWith(
-          status: PostStatus.success,
-          posts: posts,
-          hasReachedMax: false,
-        ));
-      }
-      final posts = await _fetchPosts(state.posts.length);
-      emit(posts.isEmpty
-          ? state.copyWith(hasReachedMax: true)
-          : state.copyWith(
-              status: PostStatus.success,
-              posts: List.of(state.posts)..addAll(posts),
-              hasReachedMax: false,
-            ));
-    } catch (_) {
-      emit(state.copyWith(status: PostStatus.failure));
-    }
-  }
+  // Future<void> _onPostFetched(
+  //     PostFetched event, Emitter<PostState> emit) async {
+
+  // }
 
   Future<List<Post>> _fetchPosts([int startIndex = 0]) async {
     final response = await httpClient.get(
